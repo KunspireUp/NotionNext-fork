af654d7a9ec9babe4104cd71aa75404d38589499
KunSpire <sengxi.yang@outlook.com>
2024-05-17 02:03:36 +0800

    Update style.js

--- themes/fukasawa/style.js
+++ themes/fukasawa/style.js
@@ -10,10 +10,6 @@ const Style = () => {
     body {
         background-color: #FFF
     }
-    .day body{
-        background-color: #D9AFD9;
-        background-image: linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%);
-    }
     .dark body{
         background-color: black;
     }
