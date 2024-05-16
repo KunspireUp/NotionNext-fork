a6cba16d4d42e5253bb4f32b50f99313b3db1600
KunSpire <sengxi.yang@outlook.com>
2024-05-17 01:01:15 +0800

    Update sakura.js

--- public/js/sakura.js
+++ public/js/sakura.js
@@ -123,7 +123,7 @@ function createSakura() {
       'style',
       'position: fixed;left: 0;top: 0;pointer-events: none;'
     )
-    canvas.setAttribute('id', idSakura)
+    canvas.setAttribute('id', 'canvas_sakura')
     document.getElementsByTagName('body')[0].appendChild(canvas)
     cxt = canvas.getContext('2d')
     var sakuraList = new SakuraList()
